89ba5eaa948d77c49e4a2bf113b4f719f4c804b8
Tomáš Bilder <pollib161@gmail.com>
2024-06-14 11:07:30 +0200

    Initial commit

---
+++ invoice-server-starter/src/main/java/cz/itnetwork/dto/InvoiceDTO.java
@@ -0,0 +1,4 @@
+package cz.itnetwork.dto;
+
+public class InvoiceDTO {
+}
---
+++ invoice-server-starter/src/main/java/cz/itnetwork/service/PersonService.java
@@ -0,0 +1,40 @@
+package cz.itnetwork.service;
+
+import cz.itnetwork.dto.PersonDTO;
+
+import java.util.List;
+
+public interface PersonService {
+
+    /**
+     * Creates a new person
+     *
+     * @param personDTO Person to create
+     * @return newly created person
+     */
+    PersonDTO addPerson(PersonDTO personDTO);
+
+    /**
+     * <p>Sets hidden flag to true for the person with the matching [id]</p>
+     * <p>In case a person with the passed [id] isn't found, the method <b>silently fails</b></p>
+     *
+     * @param id Person to delete
+     */
+    void removePerson(long id);
+
+    /**
+     * Fetches all non-hidden persons
+     *
+     * @return List of all non-hidden persons
+     */
+    List<PersonDTO> getAll();
+
+
+    /**
+     * Fetches the specific person by [id]
+     * In case a person with the passed [id] isn't found, the method silently fails
+     * @param id Person to look up
+     * @return Found person
+     */
+    PersonDTO getPerson(Long id);
+}
