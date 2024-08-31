02d1eae2099c20ff71eb6d5228c49b990549f8d4
Tomáš Bilder <pollib161@gmail.com>
2024-08-31 11:33:17 +0200

    UserController login, logout, GET auth

--- invoice-server-starter/src/main/java/cz/pollib/controller/UserController.java
+++ invoice-server-starter/src/main/java/cz/pollib/controller/UserController.java
@@ -1,13 +1,14 @@
 package cz.pollib.controller;
 
 import cz.pollib.dto.UserDTO;
+import cz.pollib.entity.UserEntity;
 import cz.pollib.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,8 +17,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping({"/user"})
-    public UserDTO addUser (@RequestBody @Valid UserDTO userDTO) {
+    @PostMapping("/user")
+    public UserDTO addUser(@RequestBody @Valid UserDTO userDTO) {
         return userService.createAccount(userDTO);
+    }
+
+    @PostMapping("/auth")
+    public UserDTO login(@RequestBody @Valid UserDTO userDTO, HttpServletRequest request) throws ServletException {
+        request.login(userDTO.getEmail(), userDTO.getPassword());
+
+        UserEntity user = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDTO model = new UserDTO();
+        model.setEmail(user.getEmail());
+        model.setUserId(user.getUserId());
+        model.setAdmin(user.isAdmin());
+        return model;
+    }
+
+    @DeleteMapping("/auth")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "Uživatel odhlášen";
+    }
+
+    @GetMapping("/auth")
+    public UserDTO getCurrentUser() throws ServletException{
+        try {
+            UserEntity user = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            UserDTO model = new UserDTO();
+            model.setEmail(user.getEmail());
+            model.setUserId(user.getUserId());
+            model.setAdmin(user.isAdmin());
+            return model;
+        } catch (ClassCastException e) {
+            throw new ServletException();
+        }
     }
 }
